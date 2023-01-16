221c4c1d18a98b871b6416c7a89d766b6907fee0
c-mike <claudiomichael83@gmail.com>
2023-01-16 10:21:36 -0100

    hone page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const link = require('./controllers/link_c')
 const validate = require('./middlewares/validate')
 const {add_lk, id} = require('./helpers/schemas')
 
+router.get('/',(req, res)=>{res.send("manage-links-back")})
 router.get('/all', link.all)
 router.post('/add', add_lk, validate, link.add)
 router.delete('/remove', id, validate, link.remove)
